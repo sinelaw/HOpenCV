ca4dc652c81aa8980817db0cdf3c854b2d7a5cf0
Anthony Cowley <acowley@seas.upenn.edu>
2011-02-04 19:38:42 -0500

    Added keyboard callback to namedWindow.

--- src/AI/CV/OpenCV/GUI.hs
+++ src/AI/CV/OpenCV/GUI.hs
@@ -7,18 +7,24 @@ import AI.CV.OpenCV.Core.HighGui
 import AI.CV.OpenCV.Core.CxCore (fromArr)
 import Foreign.C.String (withCString)
 
+type KeyboardCallback = Int -> IO ()
+
 -- |Create a new window with the given title. The return value is an
 -- action for destroying the window.
 namedWindow :: (HasChannels c, HasDepth d) => 
-               String -> [WindowFlag] -> Maybe MouseCallback -> 
+               String -> [WindowFlag] -> 
+               Maybe MouseCallback -> 
+               Maybe KeyboardCallback ->
                Chan (HIplImage c d) -> IO (IO ())
-namedWindow name flags _cb c = 
+namedWindow name flags _cb kcb c = 
   withCString name $ \s ->
     do cvNamedWindow s (windowFlagsToEnum flags)
        t <- forkIO $ forever (readChan c >>= 
                               flip withHIplImage (cvShowImage s . fromArr) >>
-                              waitKey 1 >> return ())
+                              waitKey 1 >>= kbWrap)
        return (killThread t >> cvDestroyWindow s)
+    where kbWrap Nothing = return ()
+          kbWrap (Just x) = maybe (return ()) ($ x) kcb
 
 -- | @waitKey delay@ waits for a key indifinitely if @delay <= 0@, or
 -- for @delay@ milliseconds. The returned value is the code of the
