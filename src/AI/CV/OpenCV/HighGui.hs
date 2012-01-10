f0cfa5758561f1563a10f837fb8ac09a227ae4eb
Chris Moline <blackredtree@gmail.com>
2012-01-09 18:04:14 -0700

    added cvNamedWindow, cvLoadImage

--- src/AI/CV/OpenCV/HighGui.hs
+++ src/AI/CV/OpenCV/HighGui.hs
@@ -72,3 +72,30 @@ foreign import ccall unsafe "HOpenCV_wrap.h show_image"
 
 foreign import ccall unsafe "highgui.h cvWaitKey"
   waitKey :: CInt -> IO CInt
+
+foreign import ccall unsafe "highgui.h cvNamedWindow"
+  cvNamedWindow :: CString -> CInt -> IO CInt
+
+type AutoSize = Bool
+
+titledWindow :: String -> AutoSize -> IO Int
+titledWindow s a
+  = do cs <- newCString s
+       i <- cvNamedWindow cs (autoSize a)
+       return $ fromToInteger i
+
+  where autoSize b = if b then 1 else 0
+
+fromToInteger :: (Integral a, Num b) => a -> b
+fromToInteger = fromInteger . toInteger
+
+newtype LoadImageColor = LoadImageColor { unLoadImageColor :: CInt }
+
+#{enum LoadImageColor, LoadImageColor
+    , loadImageColor     = CV_LOAD_IMAGE_COLOR
+    , loadImageGrayscale = CV_LOAD_IMAGE_GRAYSCALE
+    , loadImageUnchanged = CV_LOAD_IMAGE_UNCHANGED }
+
+foreign import ccall unsafe "highgui.h cvLoadImage"
+  cvLoadImage :: CString -> CInt -> IO (Ptr IplImage)
+
