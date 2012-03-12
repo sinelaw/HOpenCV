1c4d8c8b6feeaf63d3d8c1f6ef90b3ee36dac4e8
haisheng.wu <freizl@gmail.com>
2012-03-12 22:32:58 +0800

    just cp the ForeignPtrWrap

--- src/Foreign/ForeignPtrWrap.hs
+++ src/Foreign/ForeignPtrWrap.hs
@@ -2,6 +2,7 @@ module Foreign.ForeignPtrWrap where
 
 import Foreign.Ptr
 import Foreign.ForeignPtr
+
 import System.IO.Error
 
 -- | A wrapper for newForeignPtr that handles nullPtrs, and can be chained to an IO Ptr creator.
@@ -16,12 +17,7 @@ createForeignPtr dealloc allocedPtr = do
     ptr <- checkPtr allocedPtr
     newForeignPtr dealloc ptr
 
-{- 
-  Fails if the ptr is nullPtr.
-  TODO: Either instance Ptr to be MonadPlus/Sth. similar?
-        Or separate logic into two functios:
-           g = Ptr a -> Ptr a, then checkPtr = fmap . g
--}    
+-- | Fails if the ptr is nullPtr
 checkPtr :: IO (Ptr a) -> IO (Ptr a)
 checkPtr x = do 
   res <- x
@@ -29,7 +25,7 @@ checkPtr x = do
     then return res 
     else fail "Null Pointer"
 
+------------------------------------------------
 -- | Names a failure
 errorName :: String -> IO a -> IO a
 errorName = modifyIOError . const . userError
-
