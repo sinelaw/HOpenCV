5c66a8356d3a96da6f89de891c23e125bf9bb4ee
Anthony Cowley <acowley@gmail.com>
2012-03-14 21:15:49 -0400

    Added Storable utility module.

---
+++ src/AI/CV/OpenCV/Core/StorableUtil.hs
@@ -0,0 +1,7 @@
+module AI.CV.OpenCV.Core.StorableUtil where
+import Foreign.Marshal.Alloc (alloca)
+import Foreign.Storable (Storable(poke))
+import Foreign.Ptr (Ptr)
+
+withS :: Storable a => a -> (Ptr a -> IO b) -> IO b
+withS x f = alloca $ \ptr -> poke ptr x >> f ptr
