5741219d696a84225b0cf84cc5303650f1071cd1
Chris Moline <blackredtree@gmail.com>
2012-03-17 19:49:27 -0600

    utility module containing the withForeignPtr2 definition

---
+++ src/AI/CV/OpenCV/Util.hs
@@ -0,0 +1,12 @@
+module AI.CV.OpenCV.Util
+where
+
+import Foreign.Ptr
+import Foreign.ForeignPtr
+
+withForeignPtr2 :: ForeignPtr a -> ForeignPtr b
+                   -> (Ptr a -> Ptr b -> IO c)
+                   -> IO c
+withForeignPtr2 a b f
+  = let g a' = withForeignPtr b (f a')
+    in withForeignPtr a g
