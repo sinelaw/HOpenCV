7856a610a1ad093e62f953747a6ca0834feb57b3
Chris Moline <blackredtree@gmail.com>
2012-03-17 19:46:36 -0600

    made everything available from AI.CV.OpenCV so that only one import is necessary for api use

---
+++ src/AI/CV/OpenCV.hs
@@ -0,0 +1,11 @@
+module AI.CV.OpenCV
+  ( module AI.CV.OpenCV.CxCore
+  , module AI.CV.OpenCV.CV
+  , module AI.CV.OpenCV.HighGui
+  , module AI.CV.OpenCV.Video )
+where
+
+import AI.CV.OpenCV.CxCore
+import AI.CV.OpenCV.CV
+import AI.CV.OpenCV.HighGui
+import AI.CV.OpenCV.Video
